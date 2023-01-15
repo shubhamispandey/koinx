7e007f5aa5551a35050aa0ecd9efd7f34d6f95a2
Shubham <shubhampandeyhaihum@gmail.com>
2023-01-15 12:09:11 +0530

    BUG FIXED

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -67,8 +67,10 @@ const Table = ({
                 <p className="text-muted">{coin.symbol.toUpperCase()}</p>
               </td>
               <td>${coin.current_price}</td>
-              <td>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-              <td>{coin.price_change_percentage_7d_in_currency.toFixed(2)}%</td>
+              <td>{coin?.price_change_percentage_24h?.toFixed(2) || 0}%</td>
+              <td>
+                {coin?.price_change_percentage_7d_in_currency?.toFixed(2) || 0}%
+              </td>
               <td>${coin.market_cap_change_24h}</td>
               <td>{coin.total_volume}</td>
               <td>{coin.circulating_supply}</td>
